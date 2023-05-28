872b39ee58bfa3d41ccfd06d0a7dd6745de764c1
starydv7 <pawanstar72926@gmail.com>
2023-05-29 01:32:35 +0530

    registration DOne

--- client/src/pages/Auth/Register.jsx
+++ client/src/pages/Auth/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Layout from "./../../components/Layout/Layout";
+import Layout from "../../components/Layout/Layout";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import "../../styles/AuthStyles.css";
@@ -14,11 +14,10 @@ const Register = () => {
   const [answer, setAnswer] = useState("");
   const navigate = useNavigate();
 
-  // form function
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('/api/v1/auth/register', {
+      const res = await axios.post("/api/v1/auth/register", {
         name,
         email,
         password,
@@ -27,93 +26,89 @@ const Register = () => {
         answer,
       });
       if (res && res.data.success) {
-       toast.success(res.data && res.data.message);
+        toast.success(res.data && res.data.message);
         navigate("/login");
       } else {
         toast.error(res.data.message);
       }
     } catch (error) {
       console.log(error);
-        toast.error("Something went wrong");
+      toast.error("Something went wrong");
     }
   };
 
   return (
-    <Layout title="Register -Gammavit Shop Now">
-      <div className="form-container" style={{ minHeight: "90vh" }}>
-        <form onSubmit={handleSubmit}>
-          <h4 className="title">REGISTER FORM</h4>
-          <div className="mb-3">
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className="form-control"
-             
-              placeholder="Enter Your Name"
-              required
-              autoFocus
-            />
-          </div>
-          <div className="mb-3">
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="form-control"
-              
-              placeholder="Enter Your Email "
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="form-control"
-             
-              placeholder="Enter Your Password"
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <input
-              type="text"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
-              className="form-control"
-              
-              placeholder="Enter Your Phone"
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <input
-              type="text"
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-              className="form-control"
-              
-              placeholder="Enter Your Address"
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <input
-              type="text"
-              value={answer}
-              onChange={(e) => setAnswer(e.target.value)}
-              className="form-control"
-             
-              placeholder="What is Your Favorite sports"
-              required
-            />
-          </div>
-          <button type="submit" className="btn btn-primary">
-            REGISTER
-          </button>
-        </form>
+    <Layout title="Register - Gammavit Shop Now">
+      <div className="register-page">
+        <div className="form-container">
+          <form onSubmit={handleSubmit}>
+            <h4 className="title">Create an account</h4>
+            <div className="form-group">
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="form-control"
+                placeholder="Name"
+                required
+                autoFocus
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="form-control"
+                placeholder="Email"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="form-control"
+                placeholder="Password"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="text"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+                className="form-control"
+                placeholder="Phone"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="text"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+                className="form-control"
+                placeholder="Address"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="text"
+                value={answer}
+                onChange={(e) => setAnswer(e.target.value)}
+                className="form-control"
+                placeholder="What is your favorite sport?"
+                required
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              REGISTER
+            </button>
+          </form>
+        </div>
       </div>
     </Layout>
   );
