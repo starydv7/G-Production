cc0d952d9da9a179c4bb3076d0bd1982ec4104d0
starydv7 <pawanstar72926@gmail.com>
2023-06-01 01:45:06 +0530

    dashboard auth
    complete

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,8 @@ import Login from './pages/Auth/Login';
 import Dashboard from './pages/user/Dashboard';
 import PrivateRoute from './pages/Routes/Private';
 import ForgotPasssword from './pages/Auth/ForgotPassword';
+import AdminRoute from './pages/Routes/AdminRoute';
+import AdminDashboard from './pages/Admin/AdminDashboard';
 
 function App() {
   return (
@@ -17,6 +19,9 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/dashboard" element={<PrivateRoute />}>
           <Route path="" element={<Dashboard />} />
+        </Route>
+        <Route path="/dashboard" element={<AdminRoute />}>
+          <Route path="admin" element={<AdminDashboard/>}/>
         </Route>
         <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPasssword />} />
---
+++ client/src/pages/Admin/AdminDashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../components/Layout/Layout';
+
+const AdminDashboard = () => {
+  return (
+    <Layout>
+      <h2 style={{ marginTop: "100px" }}>Admin Dashboard</h2>
+    </Layout>
+  );
+}
+
+export default AdminDashboard;
