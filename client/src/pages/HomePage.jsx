f5724d2af7c7b1bb2fb1b5c2f1deb3a910a09434
starydv7 <pawanstar72926@gmail.com>
2023-06-07 23:57:06 +0530

    addtocart

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -175,7 +175,7 @@ const HomePage = () => {
                   <div className="card-name-price">
                     <button
                       className="btn btn-info ms-1"
-                      onClick={() => navigate(`/product/${p.slug}`)}
+                      onClick={() => navigate(`/products/${p.slug}`)}
                     >
                       More Details
                     </button>
