97d0d2d9ff9741236d59c0d27fd12f7a9ada1049
starydv7 <pawanstar72926@gmail.com>
2023-06-24 16:07:38 +0530

    final

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ import authRoute from "./routes/authRoute.js";
 import categoryRoute from "./routes/categoryRoutes.js";
 import productRoute from "./routes/productRoutes.js";
 import cors from "cors";
-
+import path from "path";
+const PORT = process.env.PORT || 8080;
 //config env
 dotenv.config();
 
@@ -19,17 +20,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-
+app.use(express.static(path.join(__dirname, './client/build/')));
+app.get("*", function (_,res) {
+  res.sendFile(
+    path.join(__dirname, "./client/build/index.html"),
+    function (err) {
+      res.status(500).send(err);
+  })
+})
 //routes
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/category", categoryRoute);
 app.use("/api/v1/products", productRoute);
-
-app.get("/", (req, res) => {
-  res.send("<h2>Welcome to gammavit </h2>");
-});
-
-const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(
